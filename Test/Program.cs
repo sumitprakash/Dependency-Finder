9d70318fdbe79f10a8b4ef02b186ce8bc01e5bdd
sumit prakash <sumit.prakash11@hotmail.com>
2020-09-10 22:48:48 +0530

    Removing redundant code

--- Test/Program.cs
+++ Test/Program.cs
@@ -18,8 +18,7 @@ namespace Test
             var watch = new Stopwatch();
 
             watch.Start();
-            //ExecuteSlnParser();
-            FilePermissionTest();
+            ExecuteSlnParser();
             watch.Stop();
 
             Console.WriteLine("First: " + watch.ElapsedMilliseconds);
@@ -33,128 +32,6 @@ namespace Test
             parser1.ParseSolution();
             var sln = parser1.Solution;
             CommonUtil.SlnToCSV(sln);
-        }
-
-        private static void FilePermissionTest()
-        {
-            string FileLocation = @"D:\test1";
-            DirectoryInfo info = new DirectoryInfo(FileLocation);
-            CurrentUserSecurity currentUser = new CurrentUserSecurity();
-            var x = currentUser.HasAccess(info, FileSystemRights.Read);
-            //FileIOPermission writePermission = new FileIOPermission(FileIOPermissionAccess.Write, FileLocation);
-            //var x = writePermission.ToXml();
-            //var y = writePermission.ToString();
-            //var z = writePermission.AllFiles;
-            //HasWritePermissionOnDir(FileLocation);
-        }
-
-        private static bool HasWritePermissionOnDir(string path)
-        {
-            var writeAllow = false;
-            var writeDeny = false;
-            var accessControlList = Directory.GetAccessControl(Path.GetDirectoryName(path));
-            if (accessControlList == null)
-                return false;
-            var accessRules = accessControlList.GetAccessRules(true, true,
-                                        typeof(SecurityIdentifier));
-            if (accessRules == null)
-                return false;
-
-            foreach (FileSystemAccessRule rule in accessRules)
-            {
-                if ((FileSystemRights.Write & rule.FileSystemRights) != FileSystemRights.Write)
-                    continue;
-
-                if (rule.AccessControlType == AccessControlType.Allow)
-                    writeAllow = true;
-                else if (rule.AccessControlType == AccessControlType.Deny)
-                    writeDeny = true;
-            }
-
-            return writeAllow && !writeDeny;
-        }
-
-        public class CurrentUserSecurity
-        {
-            WindowsIdentity _currentUser;
-            WindowsPrincipal _currentPrincipal;
-
-            public CurrentUserSecurity()
-            {
-                _currentUser = WindowsIdentity.GetCurrent();
-                _currentPrincipal = new WindowsPrincipal(_currentUser);
-            }
-
-            public bool HasAccess(DirectoryInfo directory, FileSystemRights right)
-            {
-                // Get the collection of authorization rules that apply to the directory.
-                AuthorizationRuleCollection acl = directory.GetAccessControl()
-                    .GetAccessRules(true, true, typeof(SecurityIdentifier));
-                return HasFileOrDirectoryAccess(right, acl);
-            }
-
-            public bool HasAccess(FileInfo file, FileSystemRights right)
-            {
-                // Get the collection of authorization rules that apply to the file.
-                AuthorizationRuleCollection acl = file.GetAccessControl(AccessControlSections.Access)
-                    .GetAccessRules(true, true, typeof(SecurityIdentifier));
-                return HasFileOrDirectoryAccess(right, acl);
-            }
-
-            private bool HasFileOrDirectoryAccess(FileSystemRights right,
-                                                  AuthorizationRuleCollection acl)
-            {
-                bool allow = false;
-                bool inheritedAllow = false;
-                bool inheritedDeny = false;
-
-                for (int i = 0; i < acl.Count; i++)
-                {
-                    var currentRule = (FileSystemAccessRule)acl[i];
-                    // If the current rule applies to the current user.
-                    if (_currentUser.User.Equals(currentRule.IdentityReference) ||
-                        _currentPrincipal.IsInRole(
-                                        (SecurityIdentifier)currentRule.IdentityReference))
-                    {
-
-                        if (currentRule.AccessControlType.Equals(AccessControlType.Deny))
-                        {
-                            if ((currentRule.FileSystemRights & right) == right)
-                            {
-                                if (currentRule.IsInherited)
-                                {
-                                    inheritedDeny = true;
-                                }
-                                else
-                                { // Non inherited "deny" takes overall precedence.
-                                    return false;
-                                }
-                            }
-                        }
-                        else if (currentRule.AccessControlType
-                                                        .Equals(AccessControlType.Allow))
-                        {
-                            if ((currentRule.FileSystemRights & right) == right)
-                            {
-                                if (currentRule.IsInherited)
-                                {
-                                    inheritedAllow = true;
-                                }
-                                else
-                                {
-                                    allow = true;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (allow)
-                { // Non inherited "allow" takes precedence over inherited rules.
-                    return true;
-                }
-                return inheritedAllow && !inheritedDeny;
-            }
         }
     }
 }
